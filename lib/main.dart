96c06413bf52f8559d862a0b71e8a314ed21bc0a
David <mail.davedev@gmail.com>
2024-01-15 11:44:28 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:web_socket_channel/html.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    final HtmlWebSocketChannel channel = HtmlWebSocketChannel.connect('ws://172.16.31.81:3000');
+
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: MyHomePage(
+        title: 'Flutter Demo Home Page',
+        channel: channel,
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title, required this.channel});
+
+  final String title;
+  final HtmlWebSocketChannel channel;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    widget.channel.stream.listen((data) {
+      setState(() {
+        _counter = int.parse(data.split(' ')[1]);
+      });
+    });
+  }
+
+  void _incrementCounter() {
+    widget.channel.sink.add('click');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    widget.channel.sink.close();
+    super.dispose();
+  }
+}
---
+++ server.dart
@@ -0,0 +1,57 @@
+import 'dart:async';
+import 'dart:io';
+
+class WebSocketServer {
+  static int counter = 0;
+  static final List<WebSocket> connectedClients = [];
+
+  static Future<void> main() async {
+    final server = await HttpServer.bind('172.16.31.81', 3000);
+    print('WebSocket server listening on port ${server.port}');
+
+    await for (var request in server) {
+      if (WebSocketTransformer.isUpgradeRequest(request)) {
+        handleWebSocket(request);
+      } else {
+        request.response.statusCode = HttpStatus.badRequest;
+        request.response.writeln('WebSocket connections only.');
+        request.response.close();
+      }
+    }
+  }
+
+  static void handleWebSocket(HttpRequest request) {
+    WebSocketTransformer.upgrade(request).then((WebSocket webSocket) {
+      connectedClients.add(webSocket);
+      print('Client connected. Total clients: ${connectedClients.length}');
+
+      webSocket.listen(
+        (data) {
+          print('Received: $data');
+          if (data == 'click') {
+            counter++;
+            updateClientsCounter();
+          }
+        },
+        onDone: () {
+          connectedClients.remove(webSocket);
+          print('Client disconnected. Total clients: ${connectedClients.length}');
+        },
+        onError: (error) {
+          print('Error: $error');
+        },
+      );
+    });
+  }
+
+  static void updateClientsCounter() {
+    final String counterMessage = 'Counter: $counter';
+    connectedClients.forEach((client) {
+      client.add(counterMessage);
+    });
+  }
+}
+
+void main() {
+  WebSocketServer.main();
+}
