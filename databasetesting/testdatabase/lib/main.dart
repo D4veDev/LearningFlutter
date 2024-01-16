ea11d5b68d9880eee87bc44a9121814413a6d3d2
David <mail.davedev@gmail.com>
2024-01-16 09:34:41 +0000

    we have made contact

--- databasetesting/testdatabase/lib/main.dart
+++ databasetesting/testdatabase/lib/main.dart
@@ -12,15 +12,14 @@ Future connectToDB() async{
 
 
   var settings = new ConnectionSettings(
-  host: 'mydatabase.cr8csc4s4i51.eu-north-1.rds.amazonaws.com', 
-  port: 3306,
-  user: 'admin',
-  password: 'databasepass',
-  db: 'mydatabase'
+    host: 'mydatabase.cr8csc4s4i51.eu-north-1.rds.amazonaws.com', 
+    port: 3306,
+    user: 'admin',
+    password: 'databasepass',
+    db: 'chinese'
   );
-var conn = await MySqlConnection.connect(settings);
-print('we have made contact');
-
+  var conn = await MySqlConnection.connect(settings);
+  print('we have made contact');
 }
 
 
